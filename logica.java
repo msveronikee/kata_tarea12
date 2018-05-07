ab09eb25fd964c30aa4fd965dacf55f00414c2fc
Verónica Lira <liramendozaveronica@gmail.com>
2018-05-07 01:14:08 -0500

    mi primer kata

--- logica.java
+++ logica.java
@@ -4,12 +4,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Vero
